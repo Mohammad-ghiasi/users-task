33d4161c3001849ec85b222f9f15ba3c29b4d518
Mohammad-ghiasi <mohammadghiasi005@gmail.com>
2025-01-24 21:15:52 +0330

    users-task-edition9

--- src/components/UserItem.tsx
+++ src/components/UserItem.tsx
@@ -34,14 +34,14 @@ export default function UserItem({
 
   return (
     <>
-      <Link href={`/users/${user._id}`}>
-        <div className="bg-white rounded-lg shadow-md hover:shadow-lg transition-shadow duration-300 ease-in-out p-4">
-          <div className="flex flex-col sm:flex-row sm:items-center sm:space-x-4 items-center">
-            <div className="w-16 h-16 sm:w-24 sm:h-24 sm:me-5 rounded-full bg-gray-200 flex items-center justify-center text-gray-500 font-bold mb-4 sm:mb-0">
-              {user.firstname[0]}
-              {user.lastname[0]}
-            </div>
+      <div className="bg-white rounded-lg shadow-md hover:shadow-lg transition-shadow duration-300 ease-in-out p-4">
+        <div className="flex flex-col sm:flex-row sm:items-center sm:space-x-4 items-center">
+          <div className="w-16 h-16 sm:w-24 sm:h-24 sm:me-5 rounded-full bg-gray-200 flex items-center justify-center text-gray-500 font-bold mb-4 sm:mb-0">
+            {user.firstname[0]}
+            {user.lastname[0]}
+          </div>
 
+          <Link href={`/users/${user._id}`}>
             <div className="flex-grow">
               <p className="text-base sm:text-lg md:text-xl font-semibold text-gray-800 truncate">
                 {`${user.firstname} ${user.lastname}`}
@@ -58,24 +58,24 @@ export default function UserItem({
                 Joined: {new Date(user.createdAt).toLocaleDateString()}
               </p>
             </div>
+          </Link>
 
-            <div className="flex flex-row space-x-5 mt-5 md:mt-0 md:flex-col items-center justify-center md:space-y-5 md:space-x-0">
-              <button
-                className="hover:scale-110 transition-all"
-                onClick={() => setIsModalOpen(true)}
-              >
-                <FaEdit color="blue" />
-              </button>
-              <button
-                className="hover:scale-110 transition-all"
-                onClick={() => onDelete(user._id)}
-              >
-                <FaTrash color="red" />
-              </button>
-            </div>
+          <div className="flex flex-row space-x-5 mt-5 md:mt-0 md:flex-col items-center justify-center md:space-y-5 md:space-x-0">
+            <button
+              className="hover:scale-110 transition-all"
+              onClick={() => setIsModalOpen(true)}
+            >
+              <FaEdit color="blue" />
+            </button>
+            <button
+              className="hover:scale-110 transition-all"
+              onClick={() => onDelete(user._id)}
+            >
+              <FaTrash color="red" />
+            </button>
           </div>
         </div>
-      </Link>
+      </div>
 
       <EditModal
         user={user}
