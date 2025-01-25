5773e6d891d5b32cb584b0619afb09ba75991561
Mohammad-ghiasi <mohammadghiasi005@gmail.com>
2025-01-25 18:16:52 +0330

    users-task-edition13-data-catching-5

--- src/utils/api.ts
+++ src/utils/api.ts
@@ -1,8 +1,8 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://localhost:3001",
-  // baseURL: "https://task-bachend.vercel.app",
+  // baseURL: "http://localhost:3001",
+  baseURL: "https://task-bachend.vercel.app",
   timeout: 15000, // 10 seconds timeout
   withCredentials: true, // Enable cookies
   headers: {
